69a842bd43d425e4bb3398bb8751b68d4427bc01
Levemannen <Levemannen@gmail.com>
2026-07-02 09:22:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "treningsbuddy-flat-v47";
+const CACHE_NAME = "treningsbuddy-flat-v50";
 const APP_SHELL = ["./", "./index.html", "./app.css", "./app.js", "./icon.svg", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
